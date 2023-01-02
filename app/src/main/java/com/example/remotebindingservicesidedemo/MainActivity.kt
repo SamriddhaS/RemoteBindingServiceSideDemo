7876b9b822783117a71c23ab13af2662e60afee8
Samriddha <samriddha97@gmail.com>
2023-01-02 23:27:47 +0530

    service class done.

--- app/src/main/java/com/example/remotebindingservicesidedemo/MainActivity.kt
+++ app/src/main/java/com/example/remotebindingservicesidedemo/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.remotebindingservicesidedemo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.remotebindingservicesidedemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var serviceIntent:Intent
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        serviceIntent = Intent(applicationContext,MyService::class.java)
+        binding.btnStart.setOnClickListener {
+            startService(serviceIntent);
+            Toast.makeText(this,"Service Started",Toast.LENGTH_SHORT).show();
+        }
+
+        binding.btnStop.setOnClickListener {
+            stopService(serviceIntent)
+        }
     }
 }
--- app/src/main/java/com/example/remotebindingservicesidedemo/MyService.kt
+++ app/src/main/java/com/example/remotebindingservicesidedemo/MyService.kt
@@ -4,6 +4,9 @@ import android.app.Service
 import android.content.Intent
 import android.os.*
 import android.util.Log
+import android.widget.Toast
+import java.util.*
+
 
 class MyService:Service() {
 
@@ -14,8 +17,7 @@ class MyService:Service() {
             override fun handleMessage(msg: Message) {
                 when (msg.what) {
                     GET_RANDOM_NUMBER_FLAG -> {
-                        val messageSendRandomNumber: Message =
-                            Message.obtain(null, GET_RANDOM_NUMBER_FLAG)
+                        val messageSendRandomNumber = Message.obtain(null, GET_RANDOM_NUMBER_FLAG)
                         messageSendRandomNumber.arg1 = getRandomNumber()
                         try {
                             msg.replyTo.send(messageSendRandomNumber)
@@ -29,10 +31,13 @@ class MyService:Service() {
         }
 
         fun getRandomNumber(): Int {
-            return 112
+            return mRandomNumber
         }
+
+        private var mRandomNumber = 0
     }
 
+    private var isRandomGenOn=false
     private val randomNumberMessenger = lazy {
         Messenger(RandomNumberRequestHandler())
     }
@@ -40,4 +45,45 @@ class MyService:Service() {
     override fun onBind(p0: Intent?): IBinder? {
         return randomNumberMessenger.value.binder
     }
+
+    override fun onRebind(intent: Intent?) {
+        super.onRebind(intent)
+    }
+
+    override fun onUnbind(intent: Intent?): Boolean {
+        return super.onUnbind(intent)
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        isRandomGenOn = true
+        Thread {
+            startRandomNumberGenerator()
+        }.start()
+        return START_STICKY
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        stopRandomNumberGenerator()
+    }
+
+    private fun startRandomNumberGenerator() {
+        while (isRandomGenOn) {
+            try {
+                Thread.sleep(1000)
+                if (isRandomGenOn) {
+                    mRandomNumber = Random().nextInt(1000) + 11
+                    Log.i(TAG, "Random Number: $mRandomNumber")
+                }
+            } catch (e: InterruptedException) {
+                Log.i(TAG, "Thread Interrupted")
+            }
+        }
+    }
+
+    private fun stopRandomNumberGenerator() {
+        isRandomGenOn = false
+        Toast.makeText(applicationContext, "Service Stopped", Toast.LENGTH_SHORT).show()
+    }
+
 }
